b45c25a3e03f1b471ad52bf4fb61dfd5c1a18e6f
zhaoyaze <zhaoyaze@52toys.com>
2021-04-29 15:24:10 +0800

    first commit

---
+++ login/src/main/module/login/LoginApplication.java
@@ -0,0 +1,16 @@
+package login;
+
+import android.app.Application;
+
+/**
+ * <pre>
+ *     author : ZYZ
+ *     e-mail : zyz163mail@163.com
+ *     time   : 2021/04/29
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class LoginApplication extends Application {
+    public static String mTestVar = "hello";
+}
